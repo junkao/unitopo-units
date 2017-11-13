5f4cd6d66f60e7c434f04f49b9bd6cacb0068db6
Maros Marsalek <mmarsalek@frinx.io>
2017-11-13 14:03:20 +0100

    Handle 0.0.0.0 in OspfAreaInterface reader
    Change-Id: I51ffd66c401fe04c71c93a106a20b7192b7ab891
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- xr-6-ospf-unit/src/main/kotlin/io/frinx/unitopo/unit/xr6/ospf/handler/AreaInterfaceReader.kt
+++ xr-6-ospf-unit/src/main/kotlin/io/frinx/unitopo/unit/xr6/ospf/handler/AreaInterfaceReader.kt
@@ -13,6 +13,8 @@ import io.fd.honeycomb.translate.read.ReadContext
 import io.fd.honeycomb.translate.read.ReadFailedException
 import io.frinx.unitopo.registry.spi.UnderlayAccess
 import io.frinx.unitopo.unit.xr6.ospf.common.OspfListReader
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ipv4.ospf.cfg.rev151109.area.table.AreaAddresses
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ipv4.ospf.cfg.rev151109.area.table.area.addresses.area.content.NameScopes
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.NetworkInstance
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.protocols.Protocol
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.ospfv2.rev170228.ospfv2.area.interfaces.structure.InterfacesBuilder
@@ -20,6 +22,7 @@ import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.ospfv2.rev170228.os
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.ospfv2.rev170228.ospfv2.area.interfaces.structure.interfaces.InterfaceBuilder
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.ospfv2.rev170228.ospfv2.area.interfaces.structure.interfaces.InterfaceKey
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.ospfv2.rev170228.ospfv2.top.ospfv2.areas.Area
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.ospfv2.rev170228.ospfv2.top.ospfv2.areas.AreaKey
 import org.opendaylight.yangtools.concepts.Builder
 import org.opendaylight.yangtools.yang.binding.DataObject
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier
@@ -32,13 +35,23 @@ class AreaInterfaceReader(private val access: UnderlayAccess) : OspfListReader.O
         val protKey = id.firstKeyOf(Protocol::class.java)
         val areaKey = id.firstKeyOf(Area::class.java)
 
-        return OspfAreaReader.getAreas(access, protKey, vrfKey.name)
-                ?.areaAreaId.orEmpty()
-                .find { areaKey.identifier.uint32 == it.areaId?.toLong() }
-                ?.nameScopes
+        val areas = OspfAreaReader.getAreas(access, protKey, vrfKey.name)
+        return findAreaNameScopes(areas, areaKey)
                 ?.nameScope.orEmpty()
                 .map { InterfaceKey(it.interfaceName.value) }
                 .toList()
+    }
+
+    private fun findAreaNameScopes(areas: AreaAddresses?, areaKey: AreaKey): NameScopes? {
+        return if (areaKey.identifier.uint32 != null) {
+            areas?.areaAreaId.orEmpty()
+                    .find { areaKey.identifier.uint32 == it.areaId?.toLong() }
+                    ?.nameScopes
+        } else {
+            areas?.areaAddress.orEmpty()
+                    .find { areaKey.identifier.dottedQuad.value == it.address?.value }
+                    ?.nameScopes
+        }
     }
 
     override fun merge(builder: Builder<out DataObject>, readData: List<Interface>) {
