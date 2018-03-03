80862228a51c789b591c3a10eeae9e905b9af983
Dana Kutenicsova <dkutenicsova@frinx.io>
2018-03-03 22:04:00 +0100

    CCASP-177 : fixed OSPF writer
    Update modifies only known attribute metric.
    
    In all methods, the remaining unsupported configuration
    that was previously on router stays untouched.
    
    Change-Id: I835fefa5410a4f3347d0532c66a6f78214689514
    Signed-off-by: Dana Kutenicsova <dkutenicsova@frinx.io>

--- junos/junos-17-ospf-unit/src/main/kotlin/io/frinx/unitopo/unit/junos/ospf/handler/OspfAreaInterfaceConfigWriter.kt
+++ junos/junos-17-ospf-unit/src/main/kotlin/io/frinx/unitopo/unit/junos/ospf/handler/OspfAreaInterfaceConfigWriter.kt
@@ -16,55 +16,50 @@ import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.ospfv2.rev170
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.ospfv2.rev170228.ospfv2.area.interfaces.structure.interfaces._interface.Config
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.ospfv2.rev170228.ospfv2.top.ospfv2.areas.Area
 import org.opendaylight.yang.gen.v1.http.yang.juniper.net.yang._1._1.jc.configuration.junos._17._3r1._10.rev170101.juniper.protocols.ospf.area.Interface as JunosInterface
-import org.opendaylight.yang.gen.v1.http.yang.juniper.net.yang._1._1.jc.configuration.junos._17._3r1._10.rev170101.juniper.protocols.ospf.area.InterfaceBuilder
+import org.opendaylight.yang.gen.v1.http.yang.juniper.net.yang._1._1.jc.configuration.junos._17._3r1._10.rev170101.juniper.protocols.ospf.area.InterfaceBuilder as JunosInterfaceBuilder
 import org.opendaylight.yang.gen.v1.http.yang.juniper.net.yang._1._1.jc.configuration.junos._17._3r1._10.rev170101.juniper.protocols.ospf.area.InterfaceKey
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier as IID
 
 class OspfAreaInterfaceConfigWriter(private val underlayAccess: UnderlayAccess) : OspfWriter<Config> {
 
     override fun updateCurrentAttributesForType(id: IID<Config>, dataBefore: Config, dataAfter: Config, writeContext: WriteContext) {
-        writeCurrentAttributesForType(id, dataAfter, writeContext);
+        writeCurrentAttributesForType(id, dataAfter, writeContext)
     }
 
     override fun writeCurrentAttributesForType(id: IID<Config>, dataAfter: Config, writeContext: WriteContext) {
-        writeData(id, dataAfter)
-    }
-
-    override fun deleteCurrentAttributesForType(id: IID<Config>, dataBefore: Config, writeContext: WriteContext) {
-        val areaId = String(id.firstKeyOf(Area::class.java).identifier.value)
-        val ifaceId = id.firstKeyOf(Interface::class.java).id
-        val ifaceIid = OspfProtocolReader.getInterfaceId(areaId, ifaceId)
-        val iface = InterfaceBuilder(underlayAccess.read(ifaceIid).checkedGet().get())
-                .setMetric(null)
-                .build()
-
+        val (underlayId, underlayIfcCfg) = getData(id, dataAfter)
         try {
-            underlayAccess.put(ifaceIid, iface)
+            underlayAccess.put(underlayId, underlayIfcCfg)
         } catch (e: Exception) {
             throw WriteFailedException(id, e)
         }
     }
 
-    private fun writeData(id: IID<Config>, data: Config) {
-        val (underlayId, underlayIfcCfg) = getData(id, data)
-
+    override fun deleteCurrentAttributesForType(id: IID<Config>, dataBefore: Config, writeContext: WriteContext) {
+        val (ifaceIid, iface) = getInterfaceBuilder(id)
+        iface.metric = null
         try {
-            underlayAccess.merge(underlayId, underlayIfcCfg)
+            underlayAccess.put(ifaceIid, iface.build())
         } catch (e: Exception) {
-            throw WriteFailedException(id, e)
+            throw WriteFailedException.DeleteFailedException(id, e)
         }
     }
 
     private fun getData(id: IID<Config>, dataAfter: Config): Pair<IID<JunosInterface>, JunosInterface> {
-        val areaId = String(id.firstKeyOf(Area::class.java).identifier.value)
-        val ifaceId = id.firstKeyOf(Interface::class.java).id
-
-        val iface = InterfaceBuilder()
-                .setKey(InterfaceKey(JunosInterface.Name(ifaceId)))
-                .setMetric(JunosInterface.Metric(dataAfter.metric.value))
-                .build()
-
-        return Pair(OspfProtocolReader.getInterfaceId(areaId, ifaceId), iface)
+        val (ifaceIid, iface) = getInterfaceBuilder(id)
+        iface.metric = JunosInterface.Metric(dataAfter.metric?.value)
+        return Pair(ifaceIid, iface.build())
     }
 
+    private fun getInterfaceBuilder(id: IID<Config>) : Pair<IID<JunosInterface>, JunosInterfaceBuilder> {
+        val areaId = String(id.firstKeyOf(Area::class.java).identifier.value)
+        val ifaceId = id.firstKeyOf(Interface::class.java).id
+        val ifaceIid = OspfProtocolReader.getInterfaceId(areaId, ifaceId)
+        val ifaceOrig = underlayAccess.read(ifaceIid).checkedGet()
+        return if (ifaceOrig.isPresent) {
+            Pair(ifaceIid, JunosInterfaceBuilder(ifaceOrig.get()).setKey(InterfaceKey(JunosInterface.Name(ifaceId))))
+        } else {
+            Pair(ifaceIid, JunosInterfaceBuilder().setKey(InterfaceKey(JunosInterface.Name(ifaceId))))
+        }
+    }
 }
