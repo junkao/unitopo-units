64b840ad09403d3201d622b05d9449dc110128a2
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2018-11-08 11:07:09 +0900

    Added yang files to junos-18-models to implement interface-unit
    Change-Id: I1c18ad5e7cc20ba8f33553e2db871868753f3328
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>

---
+++ junos/junos-18/junos-18-models/src/main/java/org/opendaylight/yang/gen/v1/http/yang/juniper/net/junos/conf/interfaces/rev180101/cos_policer_input_priority_map/Ieee8021pPremiumBuilder.java
@@ -0,0 +1,19 @@
+package org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.interfaces.rev180101.cos_policer_input_priority_map;
+import org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.interfaces.rev180101.cos_policer_input_priority_map.Ieee8021p.Premium;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class Ieee8021pPremiumBuilder {
+
+    public static org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.interfaces.rev180101.cos_policer_input_priority_map.Ieee8021p.Premium getDefaultInstance(java.lang.String defaultValue) {
+        throw new java.lang.UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ junos/junos-18/junos-18-models/src/main/java/org/opendaylight/yang/gen/v1/http/yang/juniper/net/junos/conf/interfaces/rev180101/interfaces_type/fastether/options/ieee/_802/_3ad/LacpPortPriorityBuilder.java
@@ -0,0 +1,19 @@
+package org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.interfaces.rev180101.interfaces_type.fastether.options.ieee._802._3ad;
+import org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.interfaces.rev180101.interfaces_type.fastether.options.ieee._802._3ad.Lacp.PortPriority;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class LacpPortPriorityBuilder {
+
+    public static org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.interfaces.rev180101.interfaces_type.fastether.options.ieee._802._3ad.Lacp.PortPriority getDefaultInstance(java.lang.String defaultValue) {
+        throw new java.lang.UnsupportedOperationException("Not yet implemented");
+    }
+
+}
