897a51c3f2291d85257f9a0cbfe5a331fa2f663f
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2018-12-05 17:20:52 +0900

    Updated junos-18-network-instance-unit
    Supported IIDs are below:
     - NE_NE_PR_PROTOCOL
     - NE_NE_PR_PR_LOCALAGGREGATES
     - NE_NE_PR_PR_LO_AGGREGATE
     - NE_NE_PR_PR_LO_AG_CONFIG
    
    Change-Id: I5ff9e49d04f4cae82830bfdb58ca403fca388b6b
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>

--- junos/junos-18/junos-18-network-instance-unit/src/main/kotlin/io/frinx/unitopo/unit/junos18/network/instance/Unit.kt
+++ junos/junos-18/junos-18-network-instance-unit/src/main/kotlin/io/frinx/unitopo/unit/junos18/network/instance/Unit.kt
@@ -28,13 +28,26 @@ import io.frinx.unitopo.registry.spi.UnderlayAccess
 import io.frinx.unitopo.unit.junos18.network.instance.vrf.ifc.InterfaceConfigReader
 import io.frinx.unitopo.unit.junos18.network.instance.vrf.ifc.InterfaceConfigWriter
 import io.frinx.unitopo.unit.junos18.network.instance.vrf.ifc.InterfaceReader
+import io.frinx.unitopo.unit.junos18.network.instance.vrf.protocol.ProtocolConfigWriter
+import io.frinx.unitopo.unit.junos18.network.instance.vrf.protocol.ProtocolReader
+import io.frinx.unitopo.unit.junos18.network.instance.vrf.protocol.aggregate.AggregateConfigReader
+import io.frinx.unitopo.unit.junos18.network.instance.vrf.protocol.aggregate.AggregateConfigWriter
+import io.frinx.unitopo.unit.junos18.network.instance.vrf.protocol.aggregate.AggregateReader
 import io.frinx.unitopo.unit.network.instance.NetworkInstanceUnit
 import io.frinx.unitopo.unit.utils.NoopListWriter
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.extension.rev180323.NiProtAggAug
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.local.routing.rev170515.local.aggregate.top.LocalAggregatesBuilder
 import org.opendaylight.yangtools.yang.binding.YangModuleInfo
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.local.routing.rev170515.local.aggregate.top.local.aggregates.aggregate.Config as AggregateConfig
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.Config as NetworkInstanceConfig
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.ConfigBuilder as NetworkInstanceConfigBuilder
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.extension.rev180323.`$YangModuleInfoImpl` as OpenconfigBgpExtensionYangModuleInfo
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.types.inet.rev170403.`$YangModuleInfoImpl` as OpenconfigInetTypesYangModuleInfo
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.local.routing.rev170515.`$YangModuleInfoImpl` as OpenconfigLocalRoutingYangModuleInfo
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.policy.types.rev160512.`$YangModuleInfoImpl` as OpenconfigPolicyTypesYangModuleInfo
 import org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.root.rev180101.`$YangModuleInfoImpl` as UnderlayConfRootYangModuleInfo
 import org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.routing.instances.rev180101.`$YangModuleInfoImpl` as UnderlayRoutingInstanceYangModuleInfo
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier as IID
 
 class Unit(private val registry: TranslationUnitCollector) : NetworkInstanceUnit() {
 
@@ -48,6 +61,14 @@ class Unit(private val registry: TranslationUnitCollector) : NetworkInstanceUnit
         reg?.let { reg!!.close() }
     }
 
+    override fun getYangSchemas(): Set<YangModuleInfo> =
+        setOf(
+            OpenconfigBgpExtensionYangModuleInfo.getInstance(),
+            OpenconfigInetTypesYangModuleInfo.getInstance(),
+            OpenconfigLocalRoutingYangModuleInfo.getInstance(),
+            OpenconfigPolicyTypesYangModuleInfo.getInstance()
+        ).plus(super.getYangSchemas())
+
     override fun getUnderlayYangSchemas(): Set<YangModuleInfo> = setOf(
         UnderlayConfRootYangModuleInfo.getInstance(),
         UnderlayRoutingInstanceYangModuleInfo.getInstance()
@@ -59,6 +80,12 @@ class Unit(private val registry: TranslationUnitCollector) : NetworkInstanceUnit
 
         wRegistry.add(GenericListWriter(IIDs.NE_NE_IN_INTERFACE, NoopListWriter()))
         wRegistry.add(GenericWriter(IIDs.NE_NE_IN_IN_CONFIG, InterfaceConfigWriter(underlayAccess)))
+
+        wRegistry.add(GenericWriter(IIDs.NE_NE_PR_PR_CONFIG, ProtocolConfigWriter(underlayAccess)))
+
+        wRegistry.add(GenericListWriter(IIDs.NE_NE_PR_PR_LO_AGGREGATE, NoopListWriter()))
+        wRegistry.subtreeAdd(NE_NE_PR_PR_LO_AG_CONFIG_SUBTREE,
+            GenericWriter(IIDs.NE_NE_PR_PR_LO_AG_CONFIG, AggregateConfigWriter(underlayAccess)))
     }
 
     override fun provideSpecificReaders(rRegistry: ModifiableReaderRegistryBuilder, underlayAccess: UnderlayAccess) {
@@ -68,7 +95,21 @@ class Unit(private val registry: TranslationUnitCollector) : NetworkInstanceUnit
 
         rRegistry.add(GenericConfigListReader(IIDs.NE_NE_IN_INTERFACE, InterfaceReader(underlayAccess)))
         rRegistry.add(GenericConfigReader(IIDs.NE_NE_IN_IN_CONFIG, InterfaceConfigReader(underlayAccess)))
+
+        rRegistry.add(GenericConfigListReader(IIDs.NE_NE_PR_PROTOCOL, ProtocolReader(underlayAccess)))
+        rRegistry.addStructuralReader(IIDs.NE_NE_PR_PR_LOCALAGGREGATES, LocalAggregatesBuilder::class.java)
+        rRegistry.add(GenericConfigListReader(IIDs.NE_NE_PR_PR_LO_AGGREGATE, AggregateReader(underlayAccess)))
+        rRegistry.subtreeAdd(
+            NE_NE_PR_PR_LO_AG_CONFIG_SUBTREE,
+            GenericConfigReader(IIDs.NE_NE_PR_PR_LO_AG_CONFIG, AggregateConfigReader(underlayAccess)))
     }
 
     override fun toString(): String = "Junos 18.2 network-instance translate unit"
+
+    companion object {
+        private val NE_NE_PR_PR_LO_AG_CONFIG_ROOT = IID.create(AggregateConfig::class.java)
+        private val NE_NE_PR_PR_LO_AG_CONFIG_SUBTREE = setOf(
+            NE_NE_PR_PR_LO_AG_CONFIG_ROOT.augmentation(NiProtAggAug::class.java)
+        )
+    }
 }
