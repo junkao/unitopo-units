fb77744f96b65f6b937a50f86183eceff3b42c63
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2018-12-18 11:45:26 +0900

    Added IngressAclSet handlers to junos-18-acl-unit
    Supported IIDs are below:
    -AC_IN_IN_INGRESSACLSETS
    -AC_IN_IN_IN_INGRESSACLSET
    -AC_IN_IN_IN_IN_CONFIG
    
    Available acl type is inet(ipv4) only.(inet6 is not supported)
    
    Change-Id: I05909ce3c20d4a69a96fa08cb0ba78c67105ffc4
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>

--- junos/junos-18/junos-18-acl-unit/src/main/kotlin/io/frinx/unitopo/unit/junos18/acl/Unit.kt
+++ junos/junos-18/junos-18-acl-unit/src/main/kotlin/io/frinx/unitopo/unit/junos18/acl/Unit.kt
@@ -33,9 +33,13 @@ import io.frinx.unitopo.unit.junos18.acl.handler.AclInterfaceWriter
 import io.frinx.unitopo.unit.junos18.acl.handler.EgressAclSetConfigReader
 import io.frinx.unitopo.unit.junos18.acl.handler.EgressAclSetConfigWriter
 import io.frinx.unitopo.unit.junos18.acl.handler.EgressAclSetReader
+import io.frinx.unitopo.unit.junos18.acl.handler.IngressAclSetConfigReader
+import io.frinx.unitopo.unit.junos18.acl.handler.IngressAclSetConfigWriter
+import io.frinx.unitopo.unit.junos18.acl.handler.IngressAclSetReader
 import io.frinx.unitopo.unit.utils.NoopListWriter
 import io.frinx.unitopo.unit.utils.NoopWriter
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526._interface.egress.acl.top.EgressAclSetsBuilder
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526._interface.ingress.acl.top.IngressAclSetsBuilder
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526.acl.interfaces.top.InterfacesBuilder
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526.acl.top.AclBuilder
 import io.frinx.openconfig.openconfig.interfaces.IIDs as InterfaceIIDs
@@ -78,6 +82,10 @@ class Unit(private val registry: TranslationUnitCollector) : TranslateUnit {
         wRegistry.add(GenericWriter(IIDs.AC_INTERFACES, NoopWriter()))
         wRegistry.add(GenericListWriter(IIDs.AC_IN_INTERFACE, AclInterfaceWriter()))
         wRegistry.addAfter(GenericWriter(IIDs.AC_IN_IN_CONFIG, NoopWriter()), InterfaceIIDs.IN_IN_SU_SU_CONFIG)
+        wRegistry.add(GenericWriter(IIDs.AC_IN_IN_INGRESSACLSETS, NoopWriter()))
+        wRegistry.add(GenericListWriter(IIDs.AC_IN_IN_IN_INGRESSACLSET, NoopListWriter()))
+        wRegistry.addAfter(GenericWriter(IIDs.AC_IN_IN_IN_IN_CONFIG, IngressAclSetConfigWriter(underlayAccess)),
+            IIDs.AC_IN_IN_CONFIG)
         wRegistry.add(GenericWriter(IIDs.AC_IN_IN_EGRESSACLSETS, NoopWriter()))
         wRegistry.add(GenericListWriter(IIDs.AC_IN_IN_EG_EGRESSACLSET, NoopListWriter()))
         wRegistry.addAfter(GenericWriter(IIDs.AC_IN_IN_EG_EG_CONFIG, EgressAclSetConfigWriter(underlayAccess)),
@@ -89,6 +97,9 @@ class Unit(private val registry: TranslationUnitCollector) : TranslateUnit {
         rRegistry.addStructuralReader(IIDs.AC_INTERFACES, InterfacesBuilder::class.java)
         rRegistry.add(GenericConfigListReader(IIDs.AC_IN_INTERFACE, AclInterfaceReader(underlayAccess)))
         rRegistry.add(GenericConfigReader(IIDs.AC_IN_IN_CONFIG, AclInterfaceConfigReader()))
+        rRegistry.addStructuralReader(IIDs.AC_IN_IN_INGRESSACLSETS, IngressAclSetsBuilder::class.java)
+        rRegistry.add(GenericConfigListReader(IIDs.AC_IN_IN_IN_INGRESSACLSET, IngressAclSetReader(underlayAccess)))
+        rRegistry.add(GenericConfigReader(IIDs.AC_IN_IN_IN_IN_CONFIG, IngressAclSetConfigReader()))
         rRegistry.addStructuralReader(IIDs.AC_IN_IN_EGRESSACLSETS, EgressAclSetsBuilder::class.java)
         rRegistry.add(GenericConfigListReader(IIDs.AC_IN_IN_EG_EGRESSACLSET, EgressAclSetReader(underlayAccess)))
         rRegistry.add(GenericConfigReader(IIDs.AC_IN_IN_EG_EG_CONFIG, EgressAclSetConfigReader()))
