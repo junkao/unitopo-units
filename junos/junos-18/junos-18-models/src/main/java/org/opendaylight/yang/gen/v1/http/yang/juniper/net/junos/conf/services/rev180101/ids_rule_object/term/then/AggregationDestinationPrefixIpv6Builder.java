090963bb0d27581844f7dbafb847644708f623d8
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2018-12-25 11:15:06 +0900

    Add junos-conf-services.yang to junos-18-models
    Prepared for implementation of junos-18-probe-unit
    
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
    Change-Id: I7a56d39e6fe361ec3597beb99407e12c1c01775d

---
+++ junos/junos-18/junos-18-models/src/main/java/org/opendaylight/yang/gen/v1/http/yang/juniper/net/junos/conf/services/rev180101/ids_rule_object/term/then/AggregationDestinationPrefixIpv6Builder.java
@@ -0,0 +1,19 @@
+package org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.services.rev180101.ids_rule_object.term.then;
+import org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.services.rev180101.ids_rule_object.term.then.Aggregation.DestinationPrefixIpv6;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class AggregationDestinationPrefixIpv6Builder {
+
+    public static org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.services.rev180101.ids_rule_object.term.then.Aggregation.DestinationPrefixIpv6 getDefaultInstance(java.lang.String defaultValue) {
+        throw new java.lang.UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ junos/junos-18/junos-18-models/src/main/java/org/opendaylight/yang/gen/v1/http/yang/juniper/net/junos/conf/services/rev180101/nat_pool_object/port/port_choice/case_2/RangeLowBuilder.java
@@ -0,0 +1,19 @@
+package org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.services.rev180101.nat_pool_object.port.port_choice.case_2;
+import org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.services.rev180101.nat_pool_object.port.port_choice.case_2.Range.Low;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class RangeLowBuilder {
+
+    public static org.opendaylight.yang.gen.v1.http.yang.juniper.net.junos.conf.services.rev180101.nat_pool_object.port.port_choice.case_2.Range.Low getDefaultInstance(java.lang.String defaultValue) {
+        throw new java.lang.UnsupportedOperationException("Not yet implemented");
+    }
+
+}
