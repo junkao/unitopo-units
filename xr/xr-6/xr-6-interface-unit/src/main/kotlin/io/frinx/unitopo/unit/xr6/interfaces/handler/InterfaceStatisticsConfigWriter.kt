62073ef45ddbef0f5b4f215178de5e57f0e16419
tatsuso.cho <tatsuso.cho@g.softbank.co.jp>
2019-03-12 15:54:03 +0900

    Added dampening and load-interval to xr-6-interface-unit.
    Change-Id: I6760bc6d5a165503b58525c02151e641bb7ea787
    Signed-off-by: tatsuso.cho <tatsuso.cho@g.softbank.co.jp>

---
+++ xr/xr-6/xr-6-interface-unit/src/main/kotlin/io/frinx/unitopo/unit/xr6/interfaces/handler/InterfaceStatisticsConfigWriter.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.frinx.unitopo.unit.xr6.interfaces.handler
+
+import io.fd.honeycomb.translate.spi.write.WriterCustomizer
+import io.fd.honeycomb.translate.write.WriteContext
+import io.frinx.unitopo.registry.spi.UnderlayAccess
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730.InterfaceActive
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730._interface.configurations.InterfaceConfiguration
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730._interface.configurations.InterfaceConfigurationKey
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.infra.statsd.cfg.rev151109.InterfaceConfiguration1
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.infra.statsd.cfg.rev151109._interface.configurations._interface.configuration.Statistics
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.infra.statsd.cfg.rev151109._interface.configurations._interface.configuration.StatisticsBuilder
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.xr.types.rev150629.InterfaceName
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.cisco.rev171024.statistics.top.statistics.Config
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.Interface
+
+open class InterfaceStatisticsConfigWriter(private val underlayAccess: UnderlayAccess) : WriterCustomizer<Config> {
+
+    override fun writeCurrentAttributes(
+        id: org.opendaylight.yangtools.yang.binding.InstanceIdentifier<Config>,
+        dataAfter: Config,
+        wtc: WriteContext
+    ) {
+        val (underlayId, underlayIfcCfg) = getData(id, dataAfter)
+        underlayAccess.put(underlayId, underlayIfcCfg)
+    }
+
+    override fun deleteCurrentAttributes(
+        id: org.opendaylight.yangtools.yang.binding.InstanceIdentifier<Config>,
+        dataAfter: Config,
+        wtc: WriteContext
+    ) {
+        underlayAccess.delete(getId(id))
+    }
+
+    override fun updateCurrentAttributes(
+        id: org.opendaylight.yangtools.yang.binding.InstanceIdentifier<Config>,
+        dataBefore: Config,
+        dataAfter: Config,
+        writeContext: WriteContext
+    ) {
+        if (dataAfter.loadInterval != null) {
+            writeCurrentAttributes(id, dataAfter, writeContext)
+        } else {
+            deleteCurrentAttributes(id, dataBefore, writeContext)
+        }
+    }
+
+    private fun getData(
+        id: org.opendaylight.yangtools.yang.binding.InstanceIdentifier<Config>,
+        dataAfter: Config
+    ):
+        Pair<org.opendaylight.yangtools.yang.binding.InstanceIdentifier<Statistics>, Statistics> {
+        val underlayId = getId(id)
+
+        val ifcCfgBuilder = StatisticsBuilder()
+
+        val underlayIfcCfg = ifcCfgBuilder
+            .setLoadInterval(dataAfter.loadInterval)
+            .build()
+
+        return Pair(underlayId, underlayIfcCfg)
+    }
+
+    private fun getId(
+        id: org.opendaylight.yangtools.yang.binding.InstanceIdentifier<Config>
+    ): org.opendaylight.yangtools.yang.binding.InstanceIdentifier<Statistics> {
+
+        val interfaceActive = InterfaceActive("act")
+        val ifcName = InterfaceName(id.firstKeyOf(Interface::class.java).name)
+
+        val underlayId = InterfaceReader.IFC_CFGS
+            .child(InterfaceConfiguration::class.java, InterfaceConfigurationKey(interfaceActive, ifcName))
+            .augmentation(InterfaceConfiguration1::class.java)
+            .child(Statistics::class.java)
+        return underlayId
+    }
+}
---
+++ xr/xr-6/xr-6-interface-unit/src/test/kotlin/io/frinx/unitopo/unit/xr6/interfaces/handler/InterfaceStatisticsConfigReaderTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.frinx.unitopo.unit.xr6.interfaces.handler
+
+import io.fd.honeycomb.translate.read.ReadContext
+import io.frinx.unitopo.registry.spi.UnderlayAccess
+import io.frinx.unitopo.unit.utils.AbstractNetconfHandlerTest
+import io.frinx.unitopo.unit.utils.NetconfAccessHelper
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.cisco.rev171024.Interface2
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.cisco.rev171024.statistics.top.Statistics
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.cisco.rev171024.statistics.top.statistics.Config
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.cisco.rev171024.statistics.top.statistics.ConfigBuilder
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.Interfaces
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.Interface
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.InterfaceKey
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier
+
+class InterfaceStatisticsConfigReaderTest : AbstractNetconfHandlerTest() {
+    @Mock
+    private lateinit var readContext: ReadContext
+
+    private lateinit var underlayAccess: UnderlayAccess
+
+    private lateinit var target: InterfaceStatisticsConfigReader
+
+    companion object {
+        private val NC_HELPER = NetconfAccessHelper("/data_nodes.xml")
+    }
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        underlayAccess = Mockito.spy(NetconfAccessHelper(NC_HELPER))
+        target = Mockito.spy(InterfaceStatisticsConfigReader(underlayAccess))
+    }
+    @Test
+    fun testReadCurrentAttributes() {
+        val loadinterval = 60
+        val configBuilder = ConfigBuilder()
+        val id = KeyedInstanceIdentifier.create(Interfaces::class.java)
+                .child(Interface::class.java, InterfaceKey("GigabitEthernet0/0/0/0"))
+                .augmentation(Interface2::class.java).child(Statistics::class.java).child(Config::class.java)
+        target.readCurrentAttributes(id, configBuilder, readContext)
+        Assert.assertThat(configBuilder.loadInterval.toInt(), CoreMatchers.equalTo(loadinterval))
+    }
+}
