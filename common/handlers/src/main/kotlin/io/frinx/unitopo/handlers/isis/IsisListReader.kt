8dee34c575bc5234676dd7165465df4fe3a00d02
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2019-03-12 10:34:10 +0900

    Added typed handlers for IS-IS
    Change-Id: I047203716980e5d656817254506760b167a1a17b
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>

---
+++ common/handlers/src/main/kotlin/io/frinx/unitopo/handlers/isis/IsisListReader.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.unitopo.handlers.isis
+
+import io.fd.honeycomb.translate.spi.read.ConfigListReaderCustomizer
+import io.fd.honeycomb.translate.spi.read.OperListReaderCustomizer
+import io.frinx.translate.unit.commons.handler.spi.TypedListReader
+import org.opendaylight.yangtools.concepts.Builder
+import org.opendaylight.yangtools.yang.binding.DataObject
+import org.opendaylight.yangtools.yang.binding.Identifiable
+import org.opendaylight.yangtools.yang.binding.Identifier
+
+interface IsisListReader<O, K, B> : IsisReader<O, B>, TypedListReader<O, K, B>
+        where O : DataObject, O : Identifiable<K>, K : Identifier<O>, B : Builder<O> {
+
+    interface IsisConfigListReader<O : DataObject, K : Identifier<O>, B : Builder<O>> : IsisListReader<O, K, B>,
+        ConfigListReaderCustomizer<O, K, B> where O : Identifiable<K>
+
+    interface IsisOperListReader<O : DataObject, K : Identifier<O>, B : Builder<O>> : IsisListReader<O, K, B>,
+        OperListReaderCustomizer<O, K, B> where O : Identifiable<K>
+}
