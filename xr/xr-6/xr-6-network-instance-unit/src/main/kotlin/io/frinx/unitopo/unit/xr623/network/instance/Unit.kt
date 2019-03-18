549ddf25645f41999b879b049e6b1dda4011a9cf
ning.wang01 <ning.wang01@g.softbank.co.jp>
2019-03-18 18:40:25 +0900

    Added service-policy input/output to xr-6-network-instance-unit.
    Change-Id: I011795aa614904c3b103f8f0daff0af24e564de9
    Signed-off-by: ning.wang01 <ning.wang01@g.softbank.co.jp>

--- xr/xr-6/xr-6-network-instance-unit/src/main/kotlin/io/frinx/unitopo/unit/xr623/network/instance/Unit.kt
+++ xr/xr-6/xr-6-network-instance-unit/src/main/kotlin/io/frinx/unitopo/unit/xr623/network/instance/Unit.kt
@@ -21,11 +21,16 @@ import io.fd.honeycomb.translate.impl.read.GenericConfigReader
 import io.fd.honeycomb.translate.impl.write.GenericWriter
 import io.fd.honeycomb.translate.spi.builder.CustomizerAwareReadRegistryBuilder
 import io.fd.honeycomb.translate.spi.builder.CustomizerAwareWriteRegistryBuilder
+import io.fd.honeycomb.translate.util.RWUtils
+import io.frinx.openconfig.openconfig.interfaces.IIDs as InterfacesIIDs
 import io.frinx.openconfig.openconfig.network.instance.IIDs
 import io.frinx.unitopo.handlers.network.instance.NetworkInstanceUnit
 import io.frinx.unitopo.registry.api.TranslationUnitCollector
 import io.frinx.unitopo.registry.spi.UnderlayAccess
 import io.frinx.unitopo.unit.utils.NoopWriter
+import io.frinx.unitopo.unit.xr623.network.instance.policy.forwarding.PolicyForwardingInterfaceConfigReader
+import io.frinx.unitopo.unit.xr623.network.instance.policy.forwarding.PolicyForwardingInterfaceConfigWriter
+import io.frinx.unitopo.unit.xr623.network.instance.policy.forwarding.PolicyForwardingInterfaceReader
 import io.frinx.unitopo.unit.xr623.network.instance.vrf.ifc.VrfInterfaceConfigReader
 import io.frinx.unitopo.unit.xr623.network.instance.vrf.ifc.VrfInterfaceConfigWriter
 import io.frinx.unitopo.unit.xr623.network.instance.vrf.ifc.VrfInterfaceReader
@@ -35,8 +40,12 @@ import org.opendaylight.yangtools.yang.binding.YangModuleInfo
 import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.clns.isis.cfg.rev151109.`$YangModuleInfoImpl` as UnderlayIsisYangInfo
 import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.clns.isis.datatypes.rev151109.`$YangModuleInfoImpl` as UnderlayIsisTypesYangInfo
 import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730.`$YangModuleInfoImpl` as UnderlayInterfacesYangInfo
-import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.infra.rsi.cfg.rev161219.`$YangModuleInfoImpl` as UnderlayVRFYangInfo
 import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.xr.types.rev150629.`$YangModuleInfoImpl` as UnderlayTypesYangInfo
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.infra.rsi.cfg.rev161219.`$YangModuleInfoImpl` as UnderlayVRFYangInto
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.qos.ma.cfg.rev161223.`$YangModuleInfoImpl` as UnderlayQosMaCfgYangInfo
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.policy.forwarding.rev170621.`$YangModuleInfoImpl` as PfYangModuleInfoImpl
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.pf.interfaces.extension.cisco.rev171109.`$YangModuleInfoImpl` as PfNwYangModuleInfoImpl
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.`$YangModuleInfoImpl` as NetworkInstanceYangInfo
 
 open class Unit(private val registry: TranslationUnitCollector) : NetworkInstanceUnit() {
 
@@ -52,10 +61,17 @@ open class Unit(private val registry: TranslationUnitCollector) : NetworkInstanc
 
     override fun getUnderlayYangSchemas(): Set<YangModuleInfo> = setOf(
             UnderlayInterfacesYangInfo.getInstance(),
-            UnderlayVRFYangInfo.getInstance(),
             UnderlayIsisYangInfo.getInstance(),
             UnderlayIsisTypesYangInfo.getInstance(),
-            UnderlayTypesYangInfo.getInstance()
+            UnderlayTypesYangInfo.getInstance(),
+            UnderlayVRFYangInto.getInstance(),
+            UnderlayQosMaCfgYangInfo.getInstance()
+    )
+
+    override fun getYangSchemas() = setOf(
+            PfYangModuleInfoImpl.getInstance(),
+            PfNwYangModuleInfoImpl.getInstance(),
+            NetworkInstanceYangInfo.getInstance()
     )
 
     override fun provideSpecificWriters(wRegistry: CustomizerAwareWriteRegistryBuilder, underlay: UnderlayAccess) {
@@ -65,6 +81,15 @@ open class Unit(private val registry: TranslationUnitCollector) : NetworkInstanc
             IIDs.NE_NE_CONFIG)
         wRegistry.addAfter(GenericWriter(IIDs.NE_NE_PR_PR_CONFIG, ProtocolConfigWriter(underlay)),
             IIDs.NE_NE_CONFIG)
+        // PF
+        wRegistry.addNoop(IIDs.NE_NE_PO_IN_INTERFACE)
+        wRegistry.subtreeAddAfter(
+            setOf(
+                RWUtils.cutIdFromStart(IIDs.NE_NE_PO_IN_IN_CO_AUG_NIPFIFCISCOAUG,
+                        IIDs.NE_TO_NO_CO_AUG_CONFIGURATION1_NE_NE_PO_IN_IN_CONFIG)),
+                GenericWriter(IIDs.NE_NE_PO_IN_IN_CONFIG, PolicyForwardingInterfaceConfigWriter(underlay)),
+            InterfacesIIDs.IN_IN_CONFIG
+        )
     }
 
     override fun provideSpecificReaders(rRegistry: CustomizerAwareReadRegistryBuilder, underlay: UnderlayAccess) {
@@ -73,6 +98,10 @@ open class Unit(private val registry: TranslationUnitCollector) : NetworkInstanc
         rRegistry.add(GenericConfigListReader(IIDs.NE_NE_PR_PROTOCOL, ProtocolReader(underlay)))
         rRegistry.add(GenericConfigListReader(IIDs.NE_NE_IN_INTERFACE, VrfInterfaceReader(underlay)))
         rRegistry.add(GenericConfigReader(IIDs.NE_NE_IN_IN_CONFIG, VrfInterfaceConfigReader()))
+
+        // PF
+        rRegistry.add(IIDs.NE_NE_PO_IN_INTERFACE, PolicyForwardingInterfaceReader(underlay))
+        rRegistry.add(IIDs.NE_NE_PO_IN_IN_CONFIG, PolicyForwardingInterfaceConfigReader(underlay))
     }
 
     override fun toString(): String = "XR 6 (2016-12-19) network-instance translate unit"
---
+++ xr/xr-6/xr-6-network-instance-unit/src/main/kotlin/io/frinx/unitopo/unit/xr623/network/instance/policy/forwarding/PolicyForwardingInterfaceReader.kt
@@ -0,0 +1,116 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.unitopo.unit.xr623.network.instance.policy.forwarding
+
+import io.fd.honeycomb.translate.read.ReadContext
+import io.fd.honeycomb.translate.spi.read.ConfigListReaderCustomizer
+import io.frinx.openconfig.network.instance.NetworInstance
+import io.frinx.unitopo.registry.spi.UnderlayAccess
+import io.frinx.unitopo.unit.xr6.interfaces.handler.InterfaceReader
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730.InterfaceConfigurations
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730._interface.configurations.InterfaceConfiguration
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730._interface.configurations.InterfaceConfigurationBuilder
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.qos.ma.cfg.rev161223.InterfaceConfiguration1
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.qos.ma.cfg.rev161223.qos.Qos
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.xr.types.rev150629.InterfaceName
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.InterfaceId
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.NetworkInstance
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.policy.forwarding.rev170621.pf.interfaces.structural.interfaces.Interface
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.policy.forwarding.rev170621.pf.interfaces.structural.interfaces.InterfaceBuilder
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.policy.forwarding.rev170621.pf.interfaces.structural.interfaces.InterfaceKey
+import java.util.Optional
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier as IID
+
+class PolicyForwardingInterfaceReader(private val underlayAccess: UnderlayAccess) :
+    ConfigListReaderCustomizer<Interface, InterfaceKey, InterfaceBuilder> {
+    override fun getAllIds(instanceIdentifier: IID<Interface>, readContext: ReadContext): List<InterfaceKey> {
+        val vrfName = instanceIdentifier.firstKeyOf(NetworkInstance::class.java).name
+        if (vrfName != NetworInstance.DEFAULT_NETWORK_NAME) {
+            return emptyList()
+        }
+        return underlayAccess.read(IFC_CFGS, LogicalDatastoreType.CONFIGURATION)
+            .checkedGet()
+            .orNull()
+            ?.interfaceConfiguration
+            ?.filter { isSupportedInterface(it.interfaceName.value) }
+            ?.filter { hasAnyPolicy(it) }
+            ?.map { InterfaceKey(InterfaceId(it.interfaceName.value)) }
+            .orEmpty()
+    }
+
+    override fun readCurrentAttributes(
+        instanceIdentifier: IID<Interface>,
+        interfaceBuilder: InterfaceBuilder,
+        readContext: ReadContext
+    ) {
+        // Just set the name
+        interfaceBuilder.interfaceId = instanceIdentifier.firstKeyOf(Interface::class.java).interfaceId
+    }
+
+    companion object {
+        val IFC_CFGS = IID.create(InterfaceConfigurations::class.java)!!
+        fun readInterfaceCfg(
+            underlayAccess: UnderlayAccess,
+            name: String,
+            handler: (InterfaceConfiguration) -> kotlin.Unit
+        ) {
+            val configurations = underlayAccess.read(IFC_CFGS, LogicalDatastoreType.CONFIGURATION)
+                .checkedGet()
+                .orNull()
+
+            configurations?.let {
+                it.interfaceConfiguration.orEmpty()
+                    .firstOrNull { it.interfaceName.value == name }
+                    .let { handler(it ?: getDefaultIfcCfg(name)) }
+            }
+        }
+
+        private fun getDefaultIfcCfg(name: String): InterfaceConfiguration {
+            return InterfaceConfigurationBuilder().apply {
+                interfaceName = InterfaceName(name)
+                isShutdown = null
+            }.build()
+        }
+
+        fun isSupportedInterface(name: String): Boolean {
+            return when {
+                !name.startsWith("Bundle-Ether") -> false
+                InterfaceReader.isSubinterface(name) -> false
+                else -> true
+            }
+        }
+
+        private fun hasAnyPolicy(configuration: InterfaceConfiguration): Boolean {
+            val qos = configuration.getAugmentation(InterfaceConfiguration1::class.java)?.qos
+
+            return when {
+                getInputPolicy(qos).isPresent -> true
+                getOutputPolicy(qos).isPresent -> true
+                else -> false
+            }
+        }
+
+        fun getInputPolicy(qos: Qos?): Optional<String> {
+            return Optional.ofNullable(qos?.input?.servicePolicy?.getOrNull(0)?.servicePolicyName)
+        }
+
+        fun getOutputPolicy(qos: Qos?): Optional<String> {
+            return Optional.ofNullable(qos?.output?.servicePolicy?.getOrNull(0)?.servicePolicyName)
+        }
+    }
+}
