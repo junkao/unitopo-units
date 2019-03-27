6a928cabe5247c28dd2c6f77959d5eb3465aa0e4
kenshori <kensho.ri@g.softbnk.co.jp>
2019-03-27 18:02:36 +0900

    Added interface/logging configurations to xr-6.
    Change-Id: I32fad3f55572d1fe40b3fe64631e1d3647278eb8
    Signed-off-by: kenshori <kensho.ri@g.softbnk.co.jp>

---
+++ xr/xr-6/xr-6-logging-unit/src/test/kotlin/io/frinx/unitopo/unit/xr6/logging/handler/LoggingInterfacesConfigWriterTest.kt
@@ -0,0 +1,255 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.unitopo.unit.xr6.logging.handler
+
+import io.fd.honeycomb.translate.write.WriteContext
+import io.frinx.unitopo.registry.spi.UnderlayAccess
+import io.frinx.unitopo.unit.utils.AbstractNetconfHandlerTest
+import io.frinx.unitopo.unit.utils.NetconfAccessHelper
+import org.hamcrest.CoreMatchers
+import org.hamcrest.Matcher
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730.InterfaceActive
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730.InterfaceConfigurations
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730._interface.configurations.InterfaceConfiguration
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730._interface.configurations.InterfaceConfigurationBuilder
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev150730._interface.configurations.InterfaceConfigurationKey
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.xr.types.rev150629.InterfaceName
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.InterfaceId
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.logging.rev171024.logging.interfaces.structural.Interfaces
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.logging.rev171024.logging.interfaces.structural.interfaces.Interface
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.logging.rev171024.logging.interfaces.structural.interfaces.InterfaceKey
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.logging.rev171024.logging.interfaces.structural.interfaces._interface.Config
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.logging.rev171024.logging.interfaces.structural.interfaces._interface.ConfigBuilder
+import org.opendaylight.yangtools.yang.binding.DataObject
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier
+
+class LoggingInterfacesConfigWriterTest : AbstractNetconfHandlerTest() {
+    @Mock
+    private lateinit var writeContext: WriteContext
+
+    private lateinit var underlayAccess: UnderlayAccess
+
+    private lateinit var target: LoggingInterfacesConfigWriter
+
+    companion object {
+        private val NC_HELPER = NetconfAccessHelper("/data_nodes.xml")
+        private val NATIVE_ACT = InterfaceActive("act")
+
+        // Open Config
+        private val IF_NAME = "Bundle-Ether302"
+        private val IID_CONFIG = InstanceIdentifier
+            .create(Interfaces::class.java)
+            .child(Interface::class.java, InterfaceKey(InterfaceId(IF_NAME)))
+            .child(Config::class.java)
+        private val CONFIG = ConfigBuilder()
+            .setInterfaceId(InterfaceId(IF_NAME))
+            .build()
+        // netconf
+        private val NATIVE_IF_NAME = InterfaceName(IF_NAME)
+        private val NATIVE_IID: KeyedInstanceIdentifier<InterfaceConfiguration, InterfaceConfigurationKey> =
+            KeyedInstanceIdentifier
+                .create(InterfaceConfigurations::class.java)
+                .child(InterfaceConfiguration::class.java,
+                    InterfaceConfigurationKey(NATIVE_ACT, NATIVE_IF_NAME))
+        private val NATIVE_CONFIG = NC_HELPER.read(NATIVE_IID).get().get()
+
+        private val SUBIF_NAME = "Bundle-Ether303.3"
+        private val IID_SUBIF_CONFIG = InstanceIdentifier
+            .create(Interfaces::class.java)
+            .child(Interface::class.java, InterfaceKey(InterfaceId(SUBIF_NAME)))
+            .child(Config::class.java)
+        private val SUBIF_CONFIG = ConfigBuilder()
+            .setInterfaceId(InterfaceId(SUBIF_NAME))
+            .build()
+        // netconf
+        private val NATIVE_SUBIF_NAME = InterfaceName(SUBIF_NAME)
+        private val NATIVE_SUBIF_IID: KeyedInstanceIdentifier<InterfaceConfiguration, InterfaceConfigurationKey> =
+            KeyedInstanceIdentifier
+                .create(InterfaceConfigurations::class.java)
+                .child(InterfaceConfiguration::class.java,
+                    InterfaceConfigurationKey(NATIVE_ACT, NATIVE_SUBIF_NAME))
+        private val NATIVE_SUBIF_CONFIG = NC_HELPER.read(NATIVE_SUBIF_IID).get().get()
+    }
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        underlayAccess = Mockito.spy(NetconfAccessHelper(NC_HELPER))
+        target = LoggingInterfacesConfigWriter(underlayAccess)
+    }
+
+    @Test
+    fun testWriteCurrentAttributes() {
+        val config = ConfigBuilder(CONFIG)
+            .setEnabledLoggingForEvent(LoggingInterfacesReader.LINK_UP_DOWN_EVENT_LIST)
+            .build()
+
+        val expectedConfig = InterfaceConfigurationBuilder(NATIVE_CONFIG)
+            .setInterfaceModeNonPhysical(null)
+            .setInterfaceVirtual(true)
+            .setLinkStatus(true)
+            .build()
+
+        val idCap = ArgumentCaptor
+            .forClass(InstanceIdentifier::class.java) as ArgumentCaptor<InstanceIdentifier<InterfaceConfiguration>>
+        val dataCap = ArgumentCaptor
+            .forClass(DataObject::class.java) as ArgumentCaptor<InterfaceConfiguration>
+
+        Mockito.doNothing().`when`(underlayAccess).put(Mockito.any(), Mockito.any())
+
+        // test
+        target.writeCurrentAttributes(IID_CONFIG, config, writeContext)
+
+        // capture
+        Mockito.verify(underlayAccess, Mockito.times(1)).put(idCap.capture(),
+            dataCap.capture())
+
+        // verify capture-length
+        Assert.assertThat(idCap.allValues.size, CoreMatchers.`is`(1))
+        Assert.assertThat(dataCap.allValues.size, CoreMatchers.`is`(1))
+
+        // verify captured values
+        Assert.assertThat(
+            idCap.allValues[0],
+            CoreMatchers.equalTo(NATIVE_IID) as Matcher<in InstanceIdentifier<InterfaceConfiguration>>
+        )
+        Assert.assertThat(
+            dataCap.allValues[0],
+            CoreMatchers.equalTo(expectedConfig) as Matcher<in InterfaceConfiguration>
+        )
+    }
+
+    @Test
+    fun testWriteCurrentAttributesSubinterface() {
+        val config = ConfigBuilder(SUBIF_CONFIG)
+            .setEnabledLoggingForEvent(LoggingInterfacesReader.LINK_UP_DOWN_EVENT_LIST)
+            .build()
+
+        val expectedConfig = InterfaceConfigurationBuilder(NATIVE_SUBIF_CONFIG)
+            .setInterfaceModeNonPhysical(null)
+            .setInterfaceVirtual(true)
+            .setLinkStatus(true)
+            .build()
+
+        val idCap = ArgumentCaptor
+            .forClass(InstanceIdentifier::class.java) as ArgumentCaptor<InstanceIdentifier<InterfaceConfiguration>>
+        val dataCap = ArgumentCaptor
+            .forClass(DataObject::class.java) as ArgumentCaptor<InterfaceConfiguration>
+
+        Mockito.doNothing().`when`(underlayAccess).put(Mockito.any(), Mockito.any())
+
+        // test
+        target.writeCurrentAttributes(IID_SUBIF_CONFIG, config, writeContext)
+
+        // capture
+        Mockito.verify(underlayAccess, Mockito.times(1)).put(idCap.capture(),
+            dataCap.capture())
+
+        // verify capture-length
+        Assert.assertThat(idCap.allValues.size, CoreMatchers.`is`(1))
+        Assert.assertThat(dataCap.allValues.size, CoreMatchers.`is`(1))
+
+        // verify captured values
+        Assert.assertThat(
+            idCap.allValues[0],
+            CoreMatchers.equalTo(NATIVE_SUBIF_IID) as Matcher<in InstanceIdentifier<InterfaceConfiguration>>
+        )
+        Assert.assertThat(
+            dataCap.allValues[0],
+            CoreMatchers.equalTo(expectedConfig) as Matcher<in InterfaceConfiguration>
+        )
+    }
+
+    @Test
+    fun testUpdateCurrentAttributes() {
+        val configBefore = Mockito.mock(Config::class.java)
+        val config = ConfigBuilder(CONFIG)
+            .setEnabledLoggingForEvent(null)
+            .build()
+
+        val expectedConfig = InterfaceConfigurationBuilder(NATIVE_CONFIG)
+            .setInterfaceModeNonPhysical(null)
+            .setInterfaceVirtual(true)
+            .setLinkStatus(null)
+            .build()
+
+        val idCap = ArgumentCaptor
+            .forClass(InstanceIdentifier::class.java) as ArgumentCaptor<InstanceIdentifier<InterfaceConfiguration>>
+        val dataCap = ArgumentCaptor
+            .forClass(DataObject::class.java) as ArgumentCaptor<InterfaceConfiguration>
+
+        Mockito.doNothing().`when`(underlayAccess).put(Mockito.any(), Mockito.any())
+
+        // test
+        target.updateCurrentAttributes(IID_CONFIG, configBefore, config, writeContext)
+
+        // capture
+        Mockito.verify(underlayAccess, Mockito.times(1)).put(idCap.capture(),
+            dataCap.capture())
+
+        // verify capture-length
+        Assert.assertThat(idCap.allValues.size, CoreMatchers.`is`(1))
+        Assert.assertThat(dataCap.allValues.size, CoreMatchers.`is`(1))
+
+        // verify captured values
+        Assert.assertThat(
+            idCap.allValues[0],
+            CoreMatchers.equalTo(NATIVE_IID) as Matcher<in InstanceIdentifier<InterfaceConfiguration>>
+        )
+        Assert.assertThat(
+            dataCap.allValues[0],
+            CoreMatchers.equalTo(expectedConfig) as Matcher<in InterfaceConfiguration>
+        )
+    }
+
+    @Test
+    fun testDeleteCurrentAttributes() {
+        val config = ConfigBuilder(CONFIG) // not customize
+            .build()
+
+        val idCap = ArgumentCaptor
+            .forClass(InstanceIdentifier::class.java) as ArgumentCaptor<InstanceIdentifier<InterfaceConfiguration>>
+        val dataCap = ArgumentCaptor
+            .forClass(DataObject::class.java) as ArgumentCaptor<InterfaceConfiguration>
+
+        Mockito.doNothing().`when`(underlayAccess).put(Mockito.any(), Mockito.any())
+
+        // test
+        target.deleteCurrentAttributes(IID_CONFIG, config, writeContext)
+
+        // capture
+        Mockito.verify(underlayAccess, Mockito.times(1)).put(idCap.capture(),
+            dataCap.capture())
+
+        // verify capture-length
+        Assert.assertThat(idCap.allValues.size, CoreMatchers.`is`(1))
+
+        // verify captured values
+        Assert.assertThat(
+            idCap.allValues[0],
+            CoreMatchers.equalTo(NATIVE_IID) as Matcher<in InstanceIdentifier<InterfaceConfiguration>>
+        )
+    }
+}
---
+++ xr/xr-6/xr-6-logging-unit/src/test/kotlin/io/frinx/unitopo/unit/xr6/logging/handler/LoggingInterfacesReaderTest.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.unitopo.unit.xr6.logging.handler
+
+import io.fd.honeycomb.translate.read.ReadContext
+import io.frinx.openconfig.openconfig.logging.IIDs
+import io.frinx.unitopo.registry.spi.UnderlayAccess
+import io.frinx.unitopo.unit.utils.AbstractNetconfHandlerTest
+import io.frinx.unitopo.unit.utils.NetconfAccessHelper
+import org.hamcrest.CoreMatchers
+import org.hamcrest.Matchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.logging.rev171024.logging.interfaces.structural.InterfacesBuilder
+
+class LoggingInterfacesReaderTest : AbstractNetconfHandlerTest() {
+
+    @Mock
+    private lateinit var readContext: ReadContext
+
+    private lateinit var underlayAccess: UnderlayAccess
+
+    private lateinit var target: LoggingInterfacesReader
+
+    companion object {
+        private val NC_HELPER = NetconfAccessHelper("/data_nodes.xml")
+        private val NC_ALL_DISABLE = NetconfAccessHelper("/data_nodes_all_disable.xml")
+    }
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        underlayAccess = Mockito.spy(NetconfAccessHelper(NC_HELPER))
+        target = LoggingInterfacesReader(underlayAccess)
+    }
+
+    @Test
+    fun testReadCurrentAttributesNormal() {
+        val interfacesBuilder = InterfacesBuilder()
+
+        target.readCurrentAttributes(IIDs.LO_INTERFACES, interfacesBuilder, readContext)
+
+        Assert.assertThat(interfacesBuilder.`interface`.map { it.interfaceId.value },
+            Matchers.containsInAnyOrder("Bundle-Ether301", "Bundle-Ether304.4"))
+    }
+
+    @Test
+    fun testReadCurrentAttributesAllDisable() {
+        underlayAccess = Mockito.spy(NetconfAccessHelper(NC_ALL_DISABLE))
+        target = LoggingInterfacesReader(underlayAccess)
+
+        val interfacesBuilder = InterfacesBuilder()
+
+        target.readCurrentAttributes(IIDs.LO_INTERFACES, interfacesBuilder, readContext)
+
+        Assert.assertThat(interfacesBuilder.`interface`, CoreMatchers.nullValue())
+    }
+}
