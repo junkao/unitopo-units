29be1f8d92f202de2a261fc89ae82acfd7496fa5
tatsuso.cho <tatsuso.cho@g.softbank.co.jp>
2020-02-04 12:33:13 +0900

    Added initial project for IOSXR 7.0.90
    Signed-off-by: tatsuso.cho <tatsuso.cho@g.softbank.co.jp>
    Change-Id: I8957f40340990366ba4fb7762cefebaa9a627b63

---
+++ xr/xr-7/xr-7-init-unit/src/main/kotlin/io/frinx/unitopo/unit/xr7/init/Unit.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2020 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.unitopo.unit.xr7.init
+
+import io.frinx.unitopo.registry.spi.TranslateUnit
+
+/**
+ * This unit is a holder of generic settings for XR7 netconf session. No handlers defined here.
+ */
+abstract class Unit : TranslateUnit {
+
+    override fun toString(): String = "XR 7 init unit"
+}
